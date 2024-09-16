14060f7140e85f2fac9c48a5cc0a91764428f14e
Abhinav Shah <102052690+b0tAbhinaV@users.noreply.github.com>
2024-09-16 17:41:27 +0530

    Time: 11 ms (41.71%), Space: 8.5 MB (37.66%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -7,12 +7,6 @@ public:
         while(n > 0){
             int pop = n%10;
             n /= 10;
-            // if (rev > INT_MAX/10 || (rev == INT_MAX/10 && pop > 7)) {
-            //     return 0;  // Overflow for positive numbers
-            // }
-            // if (rev < INT_MIN/10 || (rev == INT_MIN/10 && pop < -8)) {
-            //     return 0;  // Overflow for negative numbers
-            // }
             rev = rev*10 + pop;
         }
         if(rev == x) return true;
