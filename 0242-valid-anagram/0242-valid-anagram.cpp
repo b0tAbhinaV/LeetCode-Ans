002f36f446e1c73f2991995b6fae0b0b0975db18
Abhinav Shah <102052690+b0tAbhinaV@users.noreply.github.com>
2024-09-20 19:51:59 +0530

    Time: 11 ms (37.31%), Space: 9.8 MB (37.59%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.length() != t.length()) return false;
-        int n = s.length();
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        for(int i=0; i<t.length(); i++){
-            if(t[i] != s[i]) return false;
+        unordered_map<char, int> count;
+
+        for(auto x : s){
+            count[x]++;
+        }
+        for(auto x : t){
+            count[x]--;
+        }
+        for(auto x : count){
+            if(x.second != 0) return false;
         }
         return true;
     }
