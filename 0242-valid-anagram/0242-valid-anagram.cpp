d3856edb4ab1c38bb8f985feb7fcadebca5c5e63
Abhinav Shah <102052690+b0tAbhinaV@users.noreply.github.com>
2024-09-20 19:49:41 +0530

    Time: 13 ms (26.29%), Space: 8.8 MB (55.34%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -4,11 +4,6 @@ public:
         if(s.length() != t.length()) return false;
         int n = s.length();
         sort(s.begin(), s.end());
-        // map<char, int> m = {"", 0};
-        // for(int i=0; i<n; i++){
-        //     m[i].first = s[i];
-        //     m[i].second += 1;
-        // }
         sort(t.begin(), t.end());
         for(int i=0; i<t.length(); i++){
             if(t[i] != s[i]) return false;
