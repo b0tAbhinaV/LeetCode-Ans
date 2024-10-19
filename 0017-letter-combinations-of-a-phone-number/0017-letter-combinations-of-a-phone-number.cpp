a09e02d4f3fb7ab14f893de33470a51094a3da79
Abhinav Shah <102052690+b0tAbhinaV@users.noreply.github.com>
2024-10-19 23:14:01 +0530

    Time: 0 ms (100%), Space: 8.3 MB (73.59%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void fun(string &digits, int n, vector<string> &dict, vector<string> &ans, int idx, string temp){
+        if(idx == n){
+            ans.push_back(temp);
+            return;
+        }
+        string val = dict[digits[idx] - '0'];
+        for(int i = 0; i<val.size(); i++){
+            temp += val[i];
+            fun(digits, n, dict, ans, idx+1, temp);
+            temp.pop_back();
+        }
+    }
+    vector<string> letterCombinations(string digits) {
+        vector<string> ans;
+        int n = digits.length();
+        vector<string> dict = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        if(n == 0) return ans;
+        fun(digits, n, dict, ans, 0, "");
+        return ans;
+    }
+};
